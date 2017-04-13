25062084d619690df97c036f75a5250697522b89
Luke Todd <luke.todd@plus.net>
2017-04-13 10:15:48 +0100

    Updated to move app into server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var path = require('path');
 var fs = require('fs');
 var router = express.Router();
 
-/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+
 router.get('/api/tasks', function(req, res, next) {
   res.sendFile(path.join(__dirname + '/db/tasks.json'));
 });
