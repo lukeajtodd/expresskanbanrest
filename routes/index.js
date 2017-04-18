ce02b0bd832ed6908961b19820571aabebfccfff
Luke Todd <luke.todd@plus.net>
2017-04-18 15:18:40 +0100

    backup system implemented

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,19 @@ router.get('/api/tasks', function(req, res, next) {
 
 router.post('/api/tasks', function(req, res, next) {
   let dbPath = path.join(__dirname + '/db/tasks.json');
-  console.log(typeof req.body);
+  let backupPath = path.join(__dirname + '/db/backup.json');
   fs.truncate(dbPath, 0, () => {
     fs.writeFile(dbPath, JSON.stringify(req.body), (err) => {
       if (err) return console.log(err);
     });
   });
+  if (Object.keys(req.body).length !== 0 && req.body.constructor === Object) {
+    fs.truncate(backupPath, 0, () => {
+      fs.writeFile(backupPath, JSON.stringify(req.body), (err) => {
+        if (err) return console.log(err);
+      });
+    });
+  }
 });
 
 module.exports = router;
